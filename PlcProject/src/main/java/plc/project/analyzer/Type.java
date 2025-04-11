a75155ef3a54a1002b8d1db59036a8233cde110f
alexmerlo0623 <a.merlo@ufl.edu>
2025-04-10 22:56:16 -0400

    Just added checkpoint 4

---
+++ PlcProject/src/main/java/plc/project/analyzer/Type.java
@@ -0,0 +1,35 @@
+package plc.project.analyzer;
+
+import java.util.List;
+
+/**
+ * IMPORTANT: DO NOT CHANGE! This file is part of the Analyzer API and should
+ * not be modified by your solution.
+ */
+public sealed interface Type {
+
+    Primitive NIL = new Primitive("Nil");
+    Primitive BOOLEAN = new Primitive("Boolean");
+    Primitive INTEGER = new Primitive("Integer");
+    Primitive DECIMAL = new Primitive("Decimal");
+    Primitive STRING = new Primitive("String");
+
+    Primitive ANY = new Primitive("Any");
+    Primitive EQUATABLE = new Primitive("Equatable");
+    Primitive COMPARABLE = new Primitive("Comparable");
+    Primitive ITERABLE = new Primitive("Iterable");
+
+    record Primitive(
+        String name
+    ) implements Type {}
+
+    record Function(
+        List<Type> parameters,
+        Type returns
+    ) implements Type {}
+
+    record Object(
+        Scope scope
+    ) implements Type {}
+
+}
